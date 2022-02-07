b928d904359160390c3b7b8bec4926173adb2f7e
GitHub <dinindunz@gmail.com>
2022-02-07 19:03:36 +1300

    update cluster

--- cdk_constructs/deploy_eks_cluster.py
+++ cdk_constructs/deploy_eks_cluster.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     aws_eks as _eks,
-    aws_ec2 as _ec2
+    aws_ec2 as _ec2,
+    aws_iam as _iam
 )
 from constructs import Construct
 
@@ -8,14 +9,36 @@ class DeployEksCluster(Construct):
 
     def __init__(self, scope: Construct, id: str, ecr_repo_name: str, image_tag: str, container_port: int, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
+        
+        #Get the VPC
+        vpc=_ec2.Vpc.from_lookup(
+          self, 'vpc',
+          is_default=True
+        )
+        
+        #Create EKS Cluster Role
+        cluster_name='%s-Cluster' % id
+        cluster_role=_iam.Role(
+          self, '%s-Role' % cluster_name,
+          assumed_by=_iam.AccountRootPrincipal()
+        )
+        
         #Deploy Cluster
         eks_cluster=_eks.Cluster(
-            self, '%s-Cluster' % id,
-            cluster_name='%s-Cluster' % id,
-            version=_eks.KubernetesVersion.V1_21,
-            default_capacity=2,
-            default_capacity_instance=_ec2.InstanceType.of(_ec2.InstanceClass.BURSTABLE2, _ec2.InstanceSize.MICRO)
+          self, cluster_name,
+          cluster_name=cluster_name,
+          vpc=vpc,
+          vpc_subnets=vpc.public_subnets,
+          version=_eks.KubernetesVersion.V1_21,
+          default_capacity_instance=_ec2.InstanceType.of(_ec2.InstanceClass.BURSTABLE2, _ec2.InstanceSize.MICRO),
+          masters_role=cluster_role
+        )
+
+        eks_cluster.add_nodegroup_capacity(
+          'node_group',
+          instance_types= _ec2.InstanceType('t2.micro'),
+          min_size=1,
+          max_size=2
         )
 
         #Deploy the App
@@ -32,3 +55,5 @@ class DeployEksCluster(Construct):
                 ]
             }
         })
+
+
